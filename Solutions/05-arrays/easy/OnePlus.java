3419e20aa20867ccdd3295d47c14e09988c96ef3
sandeep-source <newsandeepkushwaha@gmail.com>
2022-12-29 01:17:27 +0530

    Some some more question from 05-arrays

---
+++ Solutions/05-arrays/easy/OnePlus.java
@@ -0,0 +1,22 @@
+class Solution {
+    //https://leetcode.com/problems/plus-one/submissions/867114686/
+    public int[] plusOne(int[] digits) {
+        int carry=1;
+        int i=digits.length-1;
+        while(carry!=0&&i>=0){
+             int ans=digits[i]+carry;
+             digits[i]=ans%10;
+             carry=ans/10;
+             i--;
+        }
+        if(carry==1){
+            int[] ans=new int[digits.length+1];
+            ans[0]=carry;
+            for(i=0;i<digits.length;i++){
+               ans[i+1]=digits[i];
+            }
+            return ans;
+        }
+        return digits;
+    }
+}
---
+++ Solutions/05-arrays/easy/RemoveDubFromSortedArray.java
@@ -0,0 +1,16 @@
+public abstract class RemoveDubFromSortedArray {
+    class Solution {
+        //https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/867126938/
+        public int removeDuplicates(int[] nums) {
+            int cur=0;
+            for(int i=1;i<nums.length;i++){
+                if(nums[i]==nums[cur]){
+                    continue;
+                }else{
+                    nums[++cur]=nums[i];
+                }
+            }
+            return cur+1;
+        }
+    }
+}
